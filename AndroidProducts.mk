e60a80010224986bfaec9f0f8fae2e575fa57e5e
aswinop <itsaswinop@gmail.com>
2023-12-17 01:10:56 +0530

    veux: Welcome to Project Matrixx

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,14 @@
 #
 # Copyright (C) 2023 The LineageOS Project
-#               2022-2023 The Evolution X Project
+#               
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_veux.mk
+    $(LOCAL_DIR)/lineage_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_veux-user \
-    evolution_veux-userdebug \
-    evolution_veux-eng
+    lineage_veux-user \
+    lineage_veux-userdebug \
+    lineage_veux-eng
--- lineage_veux.mk
+++ lineage_veux.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2023 The LineageOS Project
-#               2022-2023 The Evolution X Project
+#               
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,20 +12,39 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-EVO_BUILD_TYPE := OFFICIAL
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_MINI_GAPPS := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+# Inherit some common Matrixx stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := evolution_veux
+# Matrixx 
+MATRIXX_BUILD_TYPE := Official
+MATRIXX_MAINTAINER := AswinOP
+MATRIXX_CHIPSET := SM6375
+MATRIXX_BATTERY := 5000mah
+MATRIXX_DISPLAY := 1080X2400
+
+# Gapps
+WITH_GAPPS := true
+EXTRA_GAPPS := true
+
+# AOSP Recovery
+TARGET_USES_AOSP_RECOVERY := true
+
+# BOOT_ANIMATION
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Nuke AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
+#Blur effect
+TARGET_ENABLE_BLUR := true	
+
+# Device identifier
+PRODUCT_NAME := lineage_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X4 Pro 5G
 
+# GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_GAPPS_ARCH := arm64
