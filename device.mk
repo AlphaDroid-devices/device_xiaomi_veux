a7dc892adec014bec7bdac8a1e8a3274c48992bc
kamikaonashi <thorschi93@gmx.at>
2023-12-26 07:35:13 +0100

    veux: import XiaomiParts and adapt for veux
    imported and adapted xiaomiparts from https://github.com/AlphaDroid-devices/device_xiaomi_sm8350-common/tree/alpha-13/parts
    
    with thermal profiles, per app refresh rate, speaker cleaner
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- device.mk
+++ device.mk
@@ -562,6 +562,10 @@ PRODUCT_PACKAGES += \
     libvndksupport \
     libvndksupport.vendor
 
+# Xiaomi Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
