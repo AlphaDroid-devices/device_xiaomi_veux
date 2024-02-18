ba3dcde3fe183e0f61a312f1bb2707cf5224db7e
MrTopia <redkarmakar@gmail.com>
2024-02-18 14:27:47 +0530

    veux: Introduce SELINUX_IGNORE_NEVERALLOWS in `BoardConfig.mk`
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,6 +172,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-09-01
 
 # Sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
